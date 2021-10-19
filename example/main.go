28080fec9f37d44453ded8956d1b8a3df541f973
duanxuelin <duanxuelin@sensoro.com>
2021-10-19 19:59:09 +0800

    fix: example delete

--- example/main.go
+++ example/main.go
@@ -25,9 +25,9 @@ type UserRequestConfig struct {
 
 type UserListRequestConfig struct {
 	cake.RequestConfig
-	Limit    int    `query:"limit"`
-	Page     int    `query:"page"`
-	XTraceID string `header:"X-Trace-Id"`
+	Limit int    `query:"limit"`
+	Page  int    `query:"page"`
+	Token string `header:"Authorization"`
 }
 
 type UserCreateRequestConfig struct {
@@ -40,7 +40,7 @@ type TestApi struct {
 	User       func(ctx context.Context, config *UserRequestConfig) (*User, error)       `url:"/users/:id" headers:"x-request-name=users;x-request-app=cake-example"`
 	Users      func(ctx context.Context, config *UserListRequestConfig) ([]*User, error) `method:"GET" url:"/users"`
 	CreateUser func(ctx context.Context, config *UserCreateRequestConfig) (*User, error) `method:"POST" url:"/users"`
-	DeleteUser func(ctx context.Context, config *UserRequestConfig) (*User, error)       `url:"/users/:id"`
+	DeleteUser func(ctx context.Context, config *UserRequestConfig) (*User, error)       `method:"DELETE" url:"/users/:id"`
 }
 
 func main() {
@@ -54,9 +54,10 @@ func main() {
 	api := apiIntf.(*TestApi)
 	newUser, err := api.CreateUser(context.Background(), &UserCreateRequestConfig{
 		Data: &User{
-			Phone:  "710-839-4565 x109",
-			Name:   "New User",
-			Avatar: "https://cdn.fakercloud.com/avatars/carlosjgsousa_128.jpg",
+			Phone:     "710-839-4565 x109",
+			Name:      "New User",
+			Avatar:    "https://cdn.fakercloud.com/avatars/carlosjgsousa_128.jpg",
+			CreatedAt: time.Now(),
 		},
 	})
 	if err != nil {
@@ -81,9 +82,9 @@ func main() {
 	}
 	fmt.Println("delete user", dUser)
 	users, err := api.Users(context.Background(), &UserListRequestConfig{
-		Limit:    10,
-		Page:     1,
-		XTraceID: "caketest1",
+		Limit: 10,
+		Page:  1,
+		Token: "Bearer Y2FrZS1leGFtcGxlLXRva2Vu",
 	})
 	if err != nil {
 		panic(err)
